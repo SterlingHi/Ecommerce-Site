61811b23c286d9bab73e4672d8eb6c69147ce7e9
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-10 16:03:38 -0500

    Added ShoppingCart model to the Models folder.

---
+++ eCommerceStarterCode/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class ShoppingCart
+    {
+        private int ShoppingCartId { get; set; }
+        private int UserId { get; set; }
+    }
+}
