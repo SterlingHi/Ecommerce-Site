bb08226e016704613e602c215faa560921ad70b1
SterlingHi <sterlinghisey@yahoo.com>
2021-08-10 13:49:15 -0500

    inital commit

---
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using eCommerceStarterCode.Configuration;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceStarterCode.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RolesConfiguration());
+        }
+
+    }
+}
